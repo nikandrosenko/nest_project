7b58a803bcebeadf55ca15e4b0c6e568c97c4d0f
Nikita Androsenko <varzaid@gmail.com>
2023-07-07 10:20:15 +0300

    Реализация очередей через модуль moderation

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,8 @@ import { Category } from './category/entities/category.entity';
 import { Post } from './posts/entities/post.entity';
 import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ModerationModule } from './moderation/moderation.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
@@ -27,6 +29,13 @@ import { AuthModule } from './auth/auth.module';
       entities: [Category, Post, User],
       synchronize: true,
     }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    ModerationModule,
     UsersModule,
     CategoryModule,
     PostsModule,
